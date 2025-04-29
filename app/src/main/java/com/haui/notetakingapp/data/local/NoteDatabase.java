f64f79b7c6e57758868f6aee3572825962c78917
ductung3008 <ductungltt@gmail.com>
2025-04-29 08:23:02 +0700

    feat: implement Note entity in Room database

--- app/src/main/java/com/haui/notetakingapp/data/local/NoteDatabase.java
+++ app/src/main/java/com/haui/notetakingapp/data/local/NoteDatabase.java
@@ -1,4 +1,29 @@
 package com.haui.notetakingapp.data.local;
 
-public class NoteDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.haui.notetakingapp.data.local.dao.NoteDao;
+import com.haui.notetakingapp.data.local.entity.Note;
+
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
+public abstract class NoteDatabase extends RoomDatabase {
+    private static NoteDatabase INSTANCE;
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            NoteDatabase.class,
+                            "note_database"
+                    ).fallbackToDestructiveMigration()
+                    .build();
+        }
+        return INSTANCE;
+    }
 }
